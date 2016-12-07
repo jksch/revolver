7d12f8f110c7efd79c084b19075e6d23e3ab7194
Jacek Solisch <jksch@e.mail.de>
2016-12-07 21:51:25 +0100

    o Refactor torch.sh for each single benchmark + gitignore

--- torch.sh
+++ torch.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 # Helper to create a flame graph with github.com/uber/go-torch.
-go test -bench . -cpuprofile=cpu.prof
-go-torch revolver.test cpu.prof
+
+mkdir -p svg
+go test -bench BenchmarkSetupDirs -cpuprofile=cpu.prof
+go-torch revolver.test cpu.prof -f svg/0_BenchmarkSetupDirs.svg 
+rm revolver.test
+rm cpu.prof
+
+mkdir -p svg
+go test -bench BenchmarkCreateFile -cpuprofile=cpu.prof
+go-torch revolver.test cpu.prof -f svg/1_BenchmarkCreateFile.svg 
+rm revolver.test
+rm cpu.prof
+
+mkdir -p svg
+go test -bench BenchmarkFileCount -cpuprofile=cpu.prof
+go-torch revolver.test cpu.prof -f svg/2_BenchmarkFileCount.svg 
+rm revolver.test
+rm cpu.prof
+
+mkdir -p svg
+go test -bench BenchmarkWriteNew -cpuprofile=cpu.prof
+go-torch revolver.test cpu.prof -f svg/3_BenchmarkWriteNew.svg 
 rm revolver.test
 rm cpu.prof
